2d5e991b3374bdd3c45bbff24a61f8de3ca1f1c4
Michael Z <michaelzwierzanski@gmail.com>
2023-05-21 15:09:11 +0100

    game complete

--- src/index.js
+++ src/index.js
@@ -39,61 +39,134 @@ const getCard = (deck) => {
   return selectedCard;
 };
 
+const setScore = (score, handArr) => {
+  for (let i = 0; i < handArr.length; i++) {
+    if (handArr[i].name === "ace") {
+      score += 1;
+    } else if (handArr[i].name === "two") {
+      score += 2;
+    } else if (handArr[i].name === "three") {
+      score += 3;
+    } else if (handArr[i].name === "four") {
+      score += 4;
+    } else if (handArr[i].name === "five") {
+      score += 5;
+    } else if (handArr[i].name === "six") {
+      score += 6;
+    } else if (handArr[i].name === "seven") {
+      score += 7;
+    } else if (handArr[i].name === "eight") {
+      score += 8;
+    } else if (handArr[i].name === "nine") {
+      score += 9;
+    } else if (handArr[i].name === "ten") {
+      score += 10;
+    } else {
+      score += 10;
+    }
+  }
+
+  return score;
+};
+
+const deal = (deck, handArr, hand) => {
+  const newCard = getCard(deck);
+  let score = 0;
+
+  deck = deck.filter((item) => item !== newCard);
+
+  handArr.push(newCard);
+
+  const cardContainer = document.createElement("div");
+  cardContainer.innerHTML = newCard.name + " " + newCard.suit;
+  hand.appendChild(cardContainer);
+
+  return score;
+};
+
+const setScoreBoard = (newScore, scoreBoard) => {
+  let gameEnd;
+  if (newScore === 21) {
+    scoreBoard.innerHTML = "You Win!";
+    gameEnd = {
+      gameOver: true,
+      win: true,
+    };
+  } else if (newScore >= 21) {
+    scoreBoard.innerHTML = "Bust!";
+    gameEnd = {
+      gameOver: true,
+      win: false,
+    };
+  } else {
+    scoreBoard.innerHTML = newScore;
+    gameEnd = {
+      gameOver: false,
+      win: false,
+    };
+  }
+  return gameEnd;
+};
+
+const loadDeck = () => {
+  let deck;
+  window.onload = deck = createDeck(suits, cards, card);
+  return deck;
+};
+
 const init = () => {
   document.addEventListener("DOMContentLoaded", () => {
-    let deck;
-    let score = 0;
-    const handArr = [];
-    console.log(score);
+    let deck = loadDeck();
+
+    let handArr = [];
+    const wins = document.getElementById("wins");
+    const losses = document.getElementById("losses");
     const scoreBoard = document.getElementById("score");
-    scoreBoard.innerHTML = score;
-    window.onload = deck = createDeck(suits, cards, card);
+    const hand = document.getElementById("hand");
+
+    scoreBoard.innerHTML = 0;
+    wins.innerHTML = 0;
+    losses.innerHTML = 0;
+
+    let winsNum = 0;
+    let lossesNum = 0;
 
     const dealBtn = document.getElementById("deal");
-
+    const resetBtn = document.getElementById("reset");
+    dealBtn.innerHTML = "Deal";
     dealBtn.addEventListener("click", () => {
-      const newCard = getCard(deck);
+      const currentHand = deal(deck, handArr, hand);
+      const newScore = setScore(currentHand, handArr);
+      const gameEnd = setScoreBoard(newScore, scoreBoard);
 
-      deck = deck.filter((item) => item !== newCard);
+      if (gameEnd.gameOver) {
+        dealBtn.classList.remove("active");
+        dealBtn.classList.add("hidden");
 
-      handArr.push(newCard);
-      console.log(handArr);
-      const cardContainer = document.createElement("div");
-      cardContainer.innerHTML = newCard.name + " " + newCard.suit;
-      document.getElementById("hand").appendChild(cardContainer);
+        resetBtn.classList.remove("hidden");
+        resetBtn.classList.add("active");
 
-      score = 0;
-      for (let i = 0; i < handArr.length; i++) {
-        if (handArr[i].name === "ace") {
-          score += 1;
-        } else if (handArr[i].name === "two") {
-          score += 2;
-        } else if (handArr[i].name === "three") {
-          score += 3;
-        } else if (handArr[i].name === "four") {
-          score += 4;
-        } else if (handArr[i].name === "five") {
-          score += 5;
-        } else if (handArr[i].name === "six") {
-          score += 6;
-        } else if (handArr[i].name === "seven") {
-          score += 7;
-        } else if (handArr[i].name === "eight") {
-          score += 8;
-        } else if (handArr[i].name === "nine") {
-          score += 9;
-        } else if (handArr[i].name === "ten") {
-          score += 10;
-        } else {
-          score += 10;
+        if (gameEnd.win === true) {
+          winsNum += 1;
+          wins.innerHTML = winsNum;
+        } else if (gameEnd.win === false) {
+          lossesNum += 1;
+          losses.innerHTML = lossesNum;
         }
       }
-      scoreBoard.innerHTML = score;
-      if (score === 21) {
-        console.log("winner");
-      } else if (score >= 21) {
-        console.log("bust");
-      }
+    });
+
+    resetBtn.addEventListener("click", () => {
+      dealBtn.classList.remove("hidden");
+      dealBtn.classList.add("active");
+
+      resetBtn.classList.remove("active");
+      resetBtn.classList.add("hidden");
+
+      handArr = [];
+      deck = loadDeck();
+      hand.innerHTML = "";
+      scoreBoard.innerHTML = 0;
     });
   });
 };
